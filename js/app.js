60bba0767dc8a7a318bee09c415e933064eafb35
ErfanKhedmati <erfankhedmati15@gmail.com>
2026-07-01 11:27:21 +0330

    update route role

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 import router from "./router.js";
 
+// application configs
+const appData = {
+    "user": {},
+    "isAuth": false,
+}
+
 document.addEventListener("click", (e)=> {
     const link = e.target.closest("[data-link]");
-
     if(!link) return;
-
     e.preventDefault();
-
     history.pushState({}, link.href);
-
-    router();
+    router(appData.isAuth);
 })
 
 window.addEventListener("popstate", router);
 
-router();
+// login at first
+router("/login")
+
+// HOME JS ----------------------------------------------
--- js/router.js
+++ js/router.js
@@ -3,11 +3,11 @@ const routes = {
     "/login": "pages/login.html"
 }
 
-export default async function router () {
-    const path = window.location.pathname;
-
+export default async function router (path) {
+    if(!path) path = window.location.pathname;
+    
     const page = routes[path];
-
+    
     const html = await fetch(page).then(res=>res.text());
     document.getElementById("app").innerHTML = html;
 }
